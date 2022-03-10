e5f355c465080de91f5ecdd52346d03cba186f74
Victor Luo <victorluo920@gmail.com>
2022-03-10 14:12:45 -0800

    managed to get sorted date to work, but how do sort by time then?

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,6 @@ import { format } from "date-fns";
 
 export async function getStaticProps() {
   let { data, error } = await supabase.from("open_mics").select("*");
-  // .order("START_DATE", { ascending: true })
-  // .limit(20);
 
   return {
     props: { data },
@@ -25,17 +23,18 @@ export default function Shows({ data }) {
       if (event.is_recurring) {
         let occurrences = getOccurrences(event.recurrence);
         let eventInstances = occurrences.map((date) => {
-          let dateString = format(date, "yyyy-MM-dd")
+          let dateString = format(date, "yyyy-MM-dd");
           return { ...event, start_date: dateString };
         });
         initialState.push(...eventInstances);
       } else {
-        initialState.push(event)
+        initialState.push(event);
       }
     });
-    initialState.sort((evt1, evt2) => { return evt1['start_date'] - evt2['start_date'] })
-    console.log(initialState)
-    setOpenMics(initialState)
+    initialState.sort((evt1, evt2) => {
+      return new Date(evt1["start_date"]) - new Date(evt2["start_date"]);
+    });
+    setOpenMics(initialState);
   }, []);
 
   return (
@@ -43,7 +42,6 @@ export default function Shows({ data }) {
       <Hero />
       <SimpleGrid columns={{ sm: 1, lg: 4 }} spacing={5}>
         {openMics.map((event, i) => (
-          // <EventListing event={event} key={event["id"]} />
           <EventCard event={event} key={i} />
         ))}
       </SimpleGrid>
