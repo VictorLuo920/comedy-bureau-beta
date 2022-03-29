74ca31923c12304e606ebd8fa644be182a751385
localoneway <andrewdelman@gmail.com>
2022-03-28 17:04:22 -0700

    Revert "More updates"
    This reverts commit 615659c551ac4b2cf2e2873e8ac42c12a0ecff92.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,7 +12,6 @@ import {
   FormLabel,
   Button,
   Menu,
-  Select,
   MenuButton,
   MenuList,
   MenuItem,
@@ -23,8 +22,7 @@ import {
   InputLeftElement,
 } from "@chakra-ui/react";
 import { ChevronDownIcon, PhoneIcon } from '@chakra-ui/icons';
-import { DropDownLocSelect, } from '/src/components/DropDownLocSelect.js'
-import { DropDownCatSelect, } from '/src/components/DropDownCatSelect.js'
+
 
 export const Hero = () => (
   <Box w="100%" px="2rem">
@@ -33,20 +31,39 @@ export const Hero = () => (
     </div>
     <Heading>Find the Funny.</Heading>
     <Text>Discover your next comedy event</Text>
-    <InputGroup w="100%" my={1} sx={{ p: 2, border: '1px dashed grey', borderRadius: "md",  }} >
+    <InputGroup w="100%" my={1}>
       <Flex w="100%" direction="column">
-              <DropDownCatSelect />
+        <Menu>
+              <MenuButton
+                px={4}
+                py={2}
+                transition='all 0.2s'
+                borderRadius='md'
+                borderWidth='1px'
+                _hover={{ bg: 'gray.400' }}
+                _expanded={{ bg: 'blue.400' }}
+                _focus={{ boxShadow: 'outline' }}
+              >
+                Category <ChevronDownIcon />
+              </MenuButton>
+                <MenuList>
+                  <MenuItem>Stand Up</MenuItem>
+                  <MenuItem>Improv</MenuItem>
+                  <MenuItem>Sketch</MenuItem>
+                  <MenuItem>Screenings/Podcasts/Panels</MenuItem>
+                  <MenuItem>Storytelling/Solo Shows</MenuItem>
+                </MenuList>
+              </Menu>
         {/* category here: this search bar needs to fire a function that returns data based on corresponding tags in organization... */}
       </Flex>
       <Flex w="100%" direction="column">
       <DatePicker />
       </Flex>
       <Flex w="100%" direction="column">
-           <DropDownLocSelect />
+        <FormLabel px="1rem">Location</FormLabel>
+        <Input borderRadius="0px" placeholder="Location" />
         {/* This corresponds to a Location field, and should refer to its own table? */}
-      </Flex>
-      <Flex w="50%" direction="column">
-      <Button size="xl">Button</Button>
+        <Button size="xl">Button</Button>
       </Flex>
     </InputGroup>
   </Box>
