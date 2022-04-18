75f2009a80ce464009c961b8b1d286f67d42236e
Victor Luo <victorluo920@gmail.com>
2022-04-18 12:17:22 -0700

    initial implementation of pagination works with some bugs like sizing and large page data fetching warning from next.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,44 +5,50 @@ import { supabase } from "../utils/supabaseClient";
 import { getOccurrences } from "../utils/dateTimeUtil";
 import { useState, useEffect } from "react";
 import { format } from "date-fns";
-import { useRouter } from 'next/router'
-import Pagination from '@etchteam/next-pagination'
+import { useRouter } from "next/router";
+import Pagination from "@etchteam/next-pagination";
 
 export async function getStaticProps() {
   let { data, error } = await supabase.from("open_mics").select("*");
- 
+
+  let computedData = [];
+  data.forEach((event) => {
+    if (event.is_recurring) {
+      let occurrences = getOccurrences(event.recurrence);
+      let eventInstances = occurrences.map((date) => {
+        let dateString = format(date, "yyyy-MM-dd");
+        return { ...event, start_date: dateString };
+      });
+      computedData.push(...eventInstances);
+    } else {
+      computedData.push(event);
+    }
+  });
+  computedData.sort((evt1, evt2) => {
+    return new Date(evt1["start_date"]) - new Date(evt2["start_date"]);
+  });
+
   return {
-    props: { data },
+    props: { computedData },
   };
 }
 
-export default function Shows({ data }) {
+export default function Shows({ computedData }) {
   let [openMics, setOpenMics] = useState([]);
-
-  const router = useRouter()
-
-  console.log(router.query) // okay, this works, query object gets console.logged with every route force change on the front end...
-
+  const router = useRouter();
+  // okay, this works, query object gets console.logged with every route force change on the front end...
   useEffect(() => {
-    let initialState = [];
-    data.forEach((event) => {
-      if (event.is_recurring) {
-        let occurrences = getOccurrences(event.recurrence);
-        let eventInstances = occurrences.map((date) => {
-          let dateString = format(date, "yyyy-MM-dd");
-          return { ...event, start_date: dateString };
-        });
-        initialState.push(...eventInstances);
-      } else {
-        initialState.push(event);
-      }
-    });
-    initialState.sort((evt1, evt2) => {
-      return new Date(evt1["start_date"]) - new Date(evt2["start_date"]);
-    });
-    setOpenMics(initialState);
-  }, []);
+    let { page = 1, size = 20 } = router.query;
 
+    let slice =
+      page <= 1
+        ? computedData.slice(0, size + 1)
+        : computedData.slice((page - 1) * size + 1, page * size + 1);
+
+    setOpenMics(slice)
+  }, [router.query]);
+
+ 
   return (
     <>
       <Hero />
@@ -51,7 +57,7 @@ export default function Shows({ data }) {
           <EventCard event={event} key={i} />
         ))}
       </SimpleGrid>
-      <Pagination total={1000} />  
+      <Pagination total={computedData.length} />
     </>
   );
 }
